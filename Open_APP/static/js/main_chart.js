adf924556326021c2b4d24974a9ba579c4bc8b61
soomin0117 <tnals9720@gmail.com>
2020-10-29 20:59:36 +0900

    주석을 추가했어요ㅏㅑ!

--- Open_APP/static/js/main_chart.js
+++ Open_APP/static/js/main_chart.js
@@ -1,3 +1,4 @@
+//=>밍지
 var config = {
 	type: 'line',
 	data: {
@@ -49,16 +50,29 @@ var config = {
 	}
 };
 
+//=>수민
+//var config_2 = {};
+//var config_3 = {};
+
+//=>고은님
+//var config_4 = {};
+
+
+
+
 window.onload = function() {
 	var chart_canvas = document.getElementById("chart_1").getContext("2d");
     var chart = new Chart(chart_canvas, config);
 
     var chart_canvas_2 = document.getElementById("chart_2").getContext("2d");
-    var chart_2 = new Chart(chart_canvas_2, config);
+	var chart_2 = new Chart(chart_canvas_2, config);
+	//config => config_2
 
     var chart_canvas_3 = document.getElementById("chart_3").getContext("2d");
-    var chart_3 = new Chart(chart_canvas_3, config);
+	var chart_3 = new Chart(chart_canvas_3, config);
+	//config => config_3
 
     var chart_canvas_4 = document.getElementById("chart_4").getContext("2d");
-    var chart_4 = new Chart(chart_canvas_4, config);
+	var chart_4 = new Chart(chart_canvas_4, config);
+	//config => config_4로 수정해서 해주세요
 };
