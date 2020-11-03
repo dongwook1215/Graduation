953ffda7f9e7df0070554093364796a8d3ba8d59
soomin0117 <tnals9720@gmail.com>
2020-11-04 02:15:22 +0900

    open_app/views.py 수정 main_chart.js 수정 open_app/main.html 수정 registerapp/static/style.css 수정

--- Open_APP/static/js/main_chart.js
+++ Open_APP/static/js/main_chart.js
@@ -68,86 +68,64 @@ var chart1_config = {
 //chart2=>
 function chart2_createChart(){
 	var chart2_country_values = [];
-	var chart2_num_of_export_values = [];
 	var chart2_amount_of_export_values = [];
+	var chart2_Totalsum_of_amount = 0;
 
 	for(var i=0; i<data2.length; i++) {
 		if(parseInt(data2[i]['기간']) == chart2_year_value){
 			chart2_country_values.push(data2[i]['국가명'])
-			chart2_num_of_export_values.push(parseInt(data2[i]['수출건수']))
 			chart2_amount_of_export_values.push(parseInt(data2[i]['수출금액']))
+			chart2_Totalsum_of_amount = parseInt(data2[i]['총수출금액'])
 		}
 	}
 
+	var chart2_sum_of_amount = chart2_amount_of_export_values.reduce((a, b) => a + b);
+
+	chart2_country_values.push('기타')
+	chart2_amount_of_export_values.push(chart2_Totalsum_of_amount-chart2_sum_of_amount)
+
 	var chart2_config = {
-		type: 'bar',
+		type: 'pie',
 		data: {
 			labels: chart2_country_values,
 			datasets: [{
-				label: '수출건수',
-				borderColor: "rgb(54, 162, 235)",
-				backgroundColor: "rgba(54, 162, 235, 0.5)",
-				data: chart2_num_of_export_values,
-				fill: false,
-				borderWidth: 2
-			}, {
-				label: '수출금액(단위: 천 불(USD 1,000))',
-				borderColor: "rgb(255, 99, 132)",
-				backgroundColor: "rgba(255, 99, 132, 0.5)",
-				data: chart2_amount_of_export_values,
-				fill: false,
-				borderWidth: 2
+				backgroundColor: ['rgb(255, 99, 99)', 'rgb(255, 161, 99)', 'rgb(255, 245, 99)', 'rgb(182, 255, 99)','rgb(99, 255, 125)','rgb(99, 245, 255)','rgb(115, 99, 255)','rgb(180, 99, 255)', 'rgb(255, 99, 190)','rgb(145, 83, 55)','rgb(151, 140, 151)'],
+				borderWidth: 2,
+				data: chart2_amount_of_export_values
 			}]
 		},
 		options: {
+			cutoutPercentage: 30,
+			legend: {
+				position:'bottom',
+				padding:5, 
+				labels: {
+					pointStyle:'circle',
+					usePointStyle:true
+				}
+			}, 
 			responsive: true,
 			title: {
 				display: true,
-				text: '[ '+chart2_year_value+'년 ]' //'Chart No.2'
+				text: '[ '+chart2_year_value+'년 ]'
 			},
 			tooltips: {
-				mode: 'index',
-				intersect: false,
 				callbacks: {
 					label: function(tooltipItem, data) {
-						return tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+						let value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+						value = value.toString();
+						value = value.split(/(?=(?:...)*$)/);
+						value = value.join(',');
+						value = data.labels[tooltipItem.index]+': '+value;
+						return value;
 					}
 				},
 			},
-			hover: {
-				mode: 'nearest',
-				intersect: true
-			},
-			scales: {
-				xAxes: [{
-					// display: true,
-					// scaleLabel: {
-					// 	display: true,
-					// 	labelString: '국가명'
-					// }
-				}],
-				yAxes: [{
-					display: true,
-					// scaleLabel: {
-					// 	display: true,
-					// 	labelString: ''
-					// },
-					ticks: {
-						beginAtZero: true,
-						userCallback: function(value, index, values) {
-							value = value.toString();
-							value = value.split(/(?=(?:...)*$)/);
-							value = value.join(',');
-							return value;
-						},
-					}
-				}]
-			}
+
 		}
 	}
 	return chart2_config;
 }
-//<=chart2
 
 function chart2_bt_clicked(chart2_bt_year, chart2_bt_id){
     document.getElementsByClassName("chart2_bt, chart2_bt_selected").item(0).className="chart2_bt";
@@ -159,73 +137,90 @@ function chart2_bt_clicked(chart2_bt_year, chart2_bt_id){
     chart_canvas2 = document.getElementById("chart2").getContext("2d");
     chart2 = new Chart(chart_canvas2, chart2_createChart());
 }
+//<=chart2
+
 
 //chart3=>
-var chart3_year_values = [];
-var chart3_country_values = [];
-var chart3_num_of_export_values = [];
-var chart3_amount_of_export_values = [];
-
-for(var i=0; i<data3.length; i++) {
-	chart3_year_values.push(parseInt(data3[i]['기간']))
-	chart3_country_values.push(data3[i]['국가명'])
-	chart3_num_of_export_values.push(parseInt(data3[i]['수출건수']))
-	chart3_amount_of_export_values.push(parseInt(data3[i]['수출금액']))
-}
-
-var chart3_config = {
-	type: 'bar',
-	data: {
-		labels: chart3_country_values,
-		datasets: [{
-			label: '수출건수',
-			borderColor: "rgb(54, 162, 235)",
- 			backgroundColor: "rgba(54, 162, 235, 0.5)",
-			data: chart3_num_of_export_values,
-			fill: false,
-			borderWidth: 2
-		}, {
-			label: '수출금액',
-			borderColor: "rgb(255, 99, 132)",
-			backgroundColor: "rgba(255, 99, 132, 0.5)",
-			data: chart3_amount_of_export_values,
-			fill: false,
-			borderWidth: 2
-		}]
-	},
-		options: {
-			responsive: true,
-			title: {
-				display: true,
-				text: 'Chart No.3'
-			},
-			tooltips: {
-				mode: 'index',
-				intersect: false,
-			},
-			hover: {
-				mode: 'nearest',
-				intersect: true
-			},
-			scales: {
-				xAxes: [{
+function chart3_createChart(){
+	var chart3_year_values = [];
+	var chart3_country_values = [];
+	var chart3_num_of_export_values = [];
+	var chart3_amount_of_export_values = [];
+	
+	for(var i=0; i<data3.length; i++) {
+		chart3_year_values.push(parseInt(data3[i]['기간']))
+		chart3_country_values.push(data3[i]['국가명'])
+		chart3_num_of_export_values.push(parseInt(data3[i]['수출건수']))
+		chart3_amount_of_export_values.push(parseInt(data3[i]['수출금액']))
+	}
+	
+	var chart3_config = {
+		type: 'bar',
+		data: {
+			labels: chart3_country_values,
+			datasets: [{
+				label: '수출건수',
+				borderColor: "rgb(54, 162, 235)",
+				 backgroundColor: "rgba(54, 162, 235, 0.5)",
+				data: chart3_num_of_export_values,
+				fill: false,
+				borderWidth: 2
+			}, {
+				label: '수출금액',
+				borderColor: "rgb(255, 99, 132)",
+				backgroundColor: "rgba(255, 99, 132, 0.5)",
+				data: chart3_amount_of_export_values,
+				fill: false,
+				borderWidth: 2
+			}]
+		},
+			options: {
+				responsive: true,
+				title: {
 					display: true,
-					scaleLabel: {
+					text: 'Chart No.3'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
 						display: true,
-						labelString: 'x'
-					}
-				}],
-				yAxes: [{
-					display: true,
-					scaleLabel: {
+						scaleLabel: {
+							display: true,
+							labelString: 'x'
+						}
+					}],
+					yAxes: [{
 						display: true,
-						labelString: 'y'
-					}
-				}]
-			}
-	 	}
+						scaleLabel: {
+							display: true,
+							labelString: 'y'
+						}
+					}]
+				}
+			 }
+		}
+		return chart3_config;
+	}
+	
+	function chart3_bt_clicked(chart3_bt_country, chart3_bt_id){
+		document.getElementsByClassName("chart3_bt, chart3_bt_selected").item(0).className="chart3_bt";
+		document.getElementById(chart3_bt_id).className = "chart3_bt, chart3_bt_selected";
+	
+		chart3_country_value=chart3_bt_country;
+		document.getElementById("chart3_chartContainer").innerHTML = '&nbsp;';
+		document.getElementById("chart3_chartContainer").innerHTML = '<canvas id="chart3"></canvas>';
+		chart_canvas3 = document.getElementById("chart3").getContext("2d");
+		chart3 = new Chart(chart_canvas3, chart3_createChart());
 	}
 //<=chart3
+
 
 //chart4=>
 var chart4_year_values = [];
@@ -304,7 +299,7 @@ window.onload = function() {
 	var chart2 = new Chart(chart_canvas2, chart2_createChart());
 
     var chart_canvas3 = document.getElementById("chart3").getContext("2d");
-    var chart3 = new Chart(chart_canvas3, chart3_config);
+    var chart3 = new Chart(chart_canvas3, chart3_createChart());
 
     var chart_canvas4 = document.getElementById("chart4").getContext("2d");
     var chart4 = new Chart(chart_canvas4, chart4_config);
